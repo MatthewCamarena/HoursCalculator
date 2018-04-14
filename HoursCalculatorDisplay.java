194dd618d3ec92b5609a7769ac6a2479682b5137
Matthew Camarena <camarena.matthew@csu.fullerton.edu>
2018-04-13 19:29:20 -0700

    Added The Main and the Display

---
+++ HoursCalculatorDisplay.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class HoursCalculatorDisplay extends JFrame
+{
+	public HoursCalculatorDisplay()
+	{
+	super("Hours Calculator");
+	}
+}
---
+++ HoursCalculatorMain.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class HoursCalculatorMain
+{
+	public static void main(String[] args)
+	{
+
+	HoursCalculatorDisplay startDisplay = new HoursCalculatorDisplay();
+
+	startDisplay.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	startDisplay.setSize(450,700);
+	startDisplay.setLocationRelativeTo(null);
+	startMain.setVisible(true);
+	}
+}
