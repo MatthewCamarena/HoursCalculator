0b6a0ffd79dec00fa64c8891ad643171b4d2baca
MatthewCamarena <camarena.matthew@csu.fullerton.edu>
2019-01-11 00:23:28 -0800

    Update and rename HoursCalculatorMain.java to HCMain.java

---
+++ HCMain.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class HCMain
+{
+	
+	public static void main(String[] args)
+	{
+
+	HCDisplay startMain = new HCDisplay();
+
+	startMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	startMain.setSize(450,700);
+	startMain.setLocationRelativeTo(null);
+	startMain.setVisible(true);
+
+	}
+
+}
